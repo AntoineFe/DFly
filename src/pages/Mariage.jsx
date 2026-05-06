3fc9db00100da900796efd499d1ad422e6089ad5
Antoine Ferrera <antoine.ferrera@yahoo.fr>
2026-05-06 10:44:24 +0200

    mosaique

--- src/pages/Mariage.jsx
+++ src/pages/Mariage.jsx
@@ -301,8 +301,8 @@ function Mariage({ lang, setLang }) {
             <GridImg src={W_IMG.dance}       cols="span 4"  rows="span 6" />
             <GridImg src={W_IMG.sortie}      cols="span 3"  rows="span 3" />
             <GridImg src={W_IMG.details}     cols="span 8"  rows="span 3" />
-            <GridImg src={W_IMG.guests}      cols="span 6"  rows="span 4" />
-            <GridImg src={W_IMG.table}       cols="span 6"  rows="span 4" />
+            <GridImg src={W_IMG.guests}      cols="span 5"  rows="span 4" />
+            <GridImg src={W_IMG.table}       cols="span 7"  rows="span 4" />
             <GridImg src={W_IMG.aerial}      cols="span 12" rows="span 4" pos="center 60%" />
           </div>
         </div>
