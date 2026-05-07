0a4c1db522264fba531e149d7bf93c4eda22ba70
Antoine Ferrera <antoine.ferrera@yahoo.fr>
2026-05-07 10:04:30 +0200

    Update Mariage.jsx

--- src/pages/Mariage.jsx
+++ src/pages/Mariage.jsx
@@ -300,7 +300,7 @@ function Mariage({ lang, setLang }) {
             <GridImg src={W_IMG.preparation} cols="span 5"  rows="span 4" />
             <GridImg src={W_IMG.dance}       cols="span 4"  rows="span 6" />
             <GridImg src={W_IMG.sortie}      cols="span 3"  rows="span 3" />
-            <GridImg src={W_IMG.details}     cols="span 8"  rows="span 3" pos="center 20%" />
+            <GridImg src={W_IMG.details}     cols="span 8"  rows="span 3" pos="center 30%" />
             <GridImg src={W_IMG.guests}      cols="span 6"  rows="span 4" />
             <GridImg src={W_IMG.table}       cols="span 6"  rows="span 4" />
             <GridImg src={W_IMG.aerial}      cols="span 12" rows="span 4" pos="center 60%" />
